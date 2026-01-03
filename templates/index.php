8635645f8342e5c7291d15e91135a000ff9b3323
Deemas97 <91252222+Deemas97@users.noreply.github.com>
2026-01-03 15:52:56 +0400

    Delete templates/index.php

--- templates/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('YADRO_PHP__ROOT_DIR', realpath(__DIR__ . '/../'));
-
-$autoloader = require (YADRO_PHP__ROOT_DIR . '/src/Bootstrap/Autoloader.php');
-
-use ZeroPHP\Kernel;
-
-return $response = (new Kernel())->handle();
